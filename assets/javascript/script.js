3df3a40e597cf864e384f0da4b2bdafae6320919
Mr. Chow <37449389+astatine305@users.noreply.github.com>
2018-06-06 19:34:36 -0700

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,20 @@
 $(document).ready(function() {
     //To hide results container on page load
     $(".resultsContainer").hide();
+    $(".resultsHeader").hide();
+
+
+    var geoUrl = "http://ip-api.com/json";
+
+    $.ajax({
+     url: geoUrl,
+     method: "GET"
+    })
+    .then(function(response){
+        console.log("geo: "+response);
+
+
+    })
 
 function getData() {
     $("#headlineBtn").on("click", function() { // START ON CLICK
@@ -11,6 +25,7 @@ function getData() {
         $("#headlineDate").empty();
     //To show results container on click    
         $(".resultsContainer").show();
+        $(".resultsHeader").show();
        
         var url = "https://newsapi.org/v2/everything?";
         var q = $("#headlineInput").val().trim();
@@ -36,29 +51,17 @@ function getData() {
 
             for (i = 0; i < response.articles.length; i++) {
                 var artTitle = $("<h2>").text("Headline: " + response.articles[i].title);
-                // artTitle.attr("target", "_blank");
-                // artTitle.attr("href", response.articles[i].url);
                 var artUrl = response.articles[i].url;
                 var artAuthor = response.articles[i].author;
                 var artSource = response.articles[i].source.name;
                 var artDate = response.articles[i].publishedAt;
 
                
-               // create separate Divs for byBlock
-
-
-
-                // var titleDiv = $("<div>");
-                // titleDiv.addClass("myTitle");
-                // titleDiv.attr("onClick", "location.href='" + response.articles[i].url+"'");
-                // titleDiv.attr("target", "_blank");
 
                 var divA = $("<a>");
                 divA.attr("href", response.articles[i].url);
                 divA.attr("target", "_blank");
                 divA.addClass("myTitle");
-
-                // var tDiv = divA.append(titleDiv);
 
 
                 var dateDiv = $("<div>");
@@ -85,9 +88,6 @@ function getData() {
 
 
           }) // End response
-
-          $("#resultsSection").css("display", "block");
-          $(".resultsHeader").css("display", "block");
 
 
     })
